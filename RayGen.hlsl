a5191fce8f42c422ff075938e5be67c8c78c6472
UtkuGokalp <38686664+UtkuGokalp@users.noreply.github.com>
2024-06-26 15:40:34 +0300

    Added some comments that explains what some stuff means as a note to my future self.

--- RayGen.hlsl
+++ RayGen.hlsl
@@ -1,10 +1,20 @@
 #include "Common.hlsl"
 
 // Raytracing output texture, accessed as a UAV
+// Unordered Access Views are for being able to read and write data to a resource
+// The read and write can be simultaneously done thanks to the underlying atomic operations
+// It also enables concurrent reads/writes through different threads.
+// This enables the updated texture to be re-used by the graphics pipeline for some other purpose.
+// u in u0 declares a UAV
 RWTexture2D<float4> gOutput : register(u0);
 
 // Raytracing acceleration structure, accessed as a SRV
+// Shader resource views are for readonly uses of a given resource.
+// These are generally used for wrapping textures in a format to which the shader can access.
+// t in t0 declares an SRV
 RaytracingAccelerationStructure SceneBVH : register(t0);
+
+// In order to declare a constant buffer (CBV), the letter c is used
 
 [shader("raygeneration")]
 void RayGen()
