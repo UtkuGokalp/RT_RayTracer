f9ea627af6b2c88153ed29f55e0ff4fa707d3380
UtkuGokalp <38686664+UtkuGokalp@users.noreply.github.com>
2024-10-20 18:54:03 +0300

    Since the issue with the renderer is fixed, objLoader library is now back in use. I have my doubts about its trustability.

--- D3D12HelloTriangle.cpp
+++ D3D12HelloTriangle.cpp
@@ -275,7 +275,7 @@ void D3D12HelloTriangle::LoadAssets()
             OBJFileManager ofm = OBJFileManager();
             std::vector<objl::Vertex> modelFileVertices;
 
-            std::string path = "rabbit.obj";
+            std::string path = "teapot.obj";
             ofm.LoadObjFile(path, modelFileVertices, indices);
 
             //Convert from objl::Vertex to Vertex struct to complete the load.
